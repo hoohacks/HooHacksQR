0fc40c027ef3bc0bc3fa1e3261b6a10f40a3c073
Rithik Yelisetty <rithik@gmail.com>
2019-02-14 21:21:20 -0500

    add os import

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 #import settings
 
 engine = create_engine(os.environ['DATABASE_URL'], convert_unicode=True)
